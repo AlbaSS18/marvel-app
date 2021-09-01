810e57b6c9fd7f4e9747463dd301111a887c35b5
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-09-01 17:24:03 +0200

    Dynamic column with v-for

--- src/components/list-character.vue
+++ src/components/list-character.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <DataTable :value="characters">
-      <Column field="Species" header="Species"></Column>
-      <Column field="Age" header="Age"></Column>
-      <Column field="Name" header="Name"></Column>
-      <Column field="Profession" header="Profession"></Column>
+      <Column v-for="col of columns" :field="col.field" :header="col.header" :key="col.field"></Column>
     </DataTable>
   </div>
 </template>
@@ -12,6 +9,16 @@
 <script>
 export default {
   name: "list-characters",
+  data(){
+      return {
+        columns: [
+          {field: 'Species', header: 'Species'},
+          {field: 'Age', header: 'Age'},
+          {field: 'Name', header: 'Name'},
+          {field: 'Profession', header: 'Profession'},
+        ]
+      }
+  },
   props: {
     characters: Array,
   },
